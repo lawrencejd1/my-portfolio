9dd582071e876be5ad209ae309ceefe2edf9ffa4
lawrencejd1 <60728037+lawrencejd1@users.noreply.github.com>
2022-09-01 15:27:44 -0600

    Fixed Responsive issue

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
       <section id="about">
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-center md:text-center mb-16 md:mb-0 items-center text-center">
+          <div className="lg:flex-grow md:flex flex-col md:items-center md:text-center mb-16 md:mb-0 items-center text-center">
             
             <h1 className="title-font sm:text-4xl text-3xl mb-10 font-medium text-white">
               Hi, I'm Jesse Lawrence.
